af1fc3a73bca3aec5082ad5d55bb7d72d1f9d995
marwaktataa <marwaktata15@gmail.com>
2019-10-22 15:21:44 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Titre from './credit-cart';
+import Gris from './zone-gris';
+import Number from './number';
+import MasterCard from './master-card';
+import Cardholder from './cardholder';
+
+
+
+tre />
+        <Gris />
+        <Number />
+        <MasterCard />
+        <Cardholder />
+      </div>
+     
+    </div>
+    
+  );
+}
+
+export default App;
